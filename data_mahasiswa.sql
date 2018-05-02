195fa1d62623294c00528b7ff7c7da216f7e64aa
septian97 <septian.rizaldi@yahoo.com>
2018-05-02 16:28:26 +0700

    upload

---
+++ data_mahasiswa.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 03 Apr 2018 pada 14.01
+-- Versi Server: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uts-web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_mahasiswa`
+--
+
+CREATE TABLE `data_mahasiswa` (
+  `nim` char(8) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data_mahasiswa`
+--
+
+INSERT INTO `data_mahasiswa` (`nim`, `nama`, `jurusan`) VALUES
+('16090075', 'Terfirman', 'TI');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_mahasiswa`
+--
+ALTER TABLE `data_mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
